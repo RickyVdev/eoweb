16a7e53aac583d1e2c5068a405b0921efbf87c34
RickyV18 <rickydvo@hotmail.com>
2025-07-21 01:03:46 -0600

    Ajustes en template agregar_empleado.html y modificar para mostrar estilos

--- eoweb/settings.py
+++ eoweb/settings.py
@@ -157,12 +157,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'gestion/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "gestion" / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

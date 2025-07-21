5af47e14f90bb4d0639c2f405f67b62489c3f606
RickyV18 <rickydvo@hotmail.com>
2025-07-21 00:55:38 -0600

    Ajustes en carpeta static para mostrar estilos

--- eoweb/settings.py
+++ eoweb/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'gestion/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
